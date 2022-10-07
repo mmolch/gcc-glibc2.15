91868e698e306d6f6ec9911cc8e3afeddc3e2120
Moritz Molch <mail@moritzmolch.de>
2022-10-07 13:58:00 +0200

    Improved chroot script

--- scripts/run-in-chroot.sh
+++ scripts/run-in-chroot.sh
@@ -15,11 +15,21 @@ _unmount() {
     done
 }
 
+case "${TARGET_MACHINE}" in
+    i686)
+        ENV_ARCH=linux32
+        ;;
+
+    x86_64)
+        ENV_ARCH=linux64
+        ;;
+esac
+
 
 _mount
 sudo mount --bind "${SOURCE_DIR}" "${TARGET_ROOT_DIR}/gcc-glibc2.15"
 sudo mount --bind "${TARGET_DIR}${INSTALL_DIR}" "${TARGET_ROOT_DIR}${INSTALL_DIR}"
-sudo chroot "${TARGET_ROOT_DIR}" linux32 "${@}"
+sudo chroot --userspec=$(id -u):$(id -g) "${TARGET_ROOT_DIR}" ${ENV_ARCH} "${@}"
 sudo umount "${TARGET_ROOT_DIR}${INSTALL_DIR}"
 sudo umount "${TARGET_ROOT_DIR}/gcc-glibc2.15"
 _unmount
